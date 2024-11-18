70ba67d985bc057b8a1b9f6db186cf8ca3d300ca
PapiCuche <carlos.curse@tecsup.edu.pe>
2024-11-18 07:04:04 -0500

    Initial Commit

---
+++ CarlosCurseSnapchatTests/CarlosCurseSnapchatTests.swift
@@ -0,0 +1,17 @@
+//
+//  CarlosCurseSnapchatTests.swift
+//  CarlosCurseSnapchatTests
+//
+//  Created by Mauricio Curse on 18/11/24.
+//
+
+import Testing
+@testable import CarlosCurseSnapchat
+
+struct CarlosCurseSnapchatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
